c6fad0a70d65f68ac48a921a79bf1f5dbbcae400
najoz <najoz@DESKTOP-IC9DKIB>
2024-03-06 22:57:52 -0600

    Avanze 3

--- src/doctor/Doctor.java
+++ src/doctor/Doctor.java
@@ -10,3 +10,21 @@ public class Doctor extends Persona {
         super(nombre, apellido, edad);
         this.especialidad = especialidad;
     }
+public class Doctor extends Persona {
+
+    private String especialidad;
+
+    public Doctor(String nombre, String apellido, int edad, String especialidad) {
+        super(nombre, apellido, edad);
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public String getProfesion() {
+        return "Doctor";
+    }
+
+    @Override
+    public void setProfesion(String profesion) {
+        // No se implementa, ya que la profesión es fija para un Doctor
+    }
