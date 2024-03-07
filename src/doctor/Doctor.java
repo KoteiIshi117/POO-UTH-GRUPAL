297fab262f94b69416dda80ce7442599316d8e19
najoz <najoz@DESKTOP-IC9DKIB>
2024-03-06 22:56:10 -0600

    Avanze 1

--- src/doctor/Doctor.java
+++ src/doctor/Doctor.java
@@ -2,6 +2,11 @@
 package doctor;
 
 
-public class Doctor {
-    
-}
+public class Doctor extends Persona {
+
+    private String especialidad;
+
+    public Doctor(String nombre, String apellido, int edad, String especialidad) {
+        super(nombre, apellido, edad);
+        this.especialidad = especialidad;
+    }
